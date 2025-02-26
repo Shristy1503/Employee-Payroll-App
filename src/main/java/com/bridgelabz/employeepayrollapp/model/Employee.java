6de39571213695cd74eb15a554d48990ecaf91fc
Shristy1503 <shriimishra15@gmail.com>
2025-02-26 16:41:02 +0530

    [Shristy] Add.auto getter setter using Lambok

--- src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
@@ -1,49 +1,26 @@
 package com.bridgelabz.employeepayrollapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import jdk.jfr.DataAmount;
+import lombok.*;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employee")
+
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Department is required")
     private String department;
+    @Min(value = 10000, message = "Salary must be at least 10,000")
     private double salary;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
