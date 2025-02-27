f3a79cee761c703eafd4ab1b8f27a9207a86b0de
Shristy1503 <shriimishra15@gmail.com>
2025-02-27 11:29:10 +0530

    [Shristy] Add.Exception handler

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty.")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and have at least 3 characters.")
     private String name;
     private double salary;
     private String department;
@@ -13,6 +16,7 @@ public class EmployeeDTO {
     public EmployeeDTO(String name, double salary, String department) {
         this.name = name;
         this.salary = salary;
+        this.department = department;
     }
 }
 
